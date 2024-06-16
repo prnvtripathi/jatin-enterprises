30f26a3c72c3823a61a2c1bd52057963d423586b
Pranav Tripathi <tripathipranav14@gmail.com>
2024-06-16 07:31:30 +0530

    chore: Update background color and add Testimonials component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
+import Why from "@/components/Why";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <About />
+      <Why />
+      <Testimonials />
     </main>
   );
 }
